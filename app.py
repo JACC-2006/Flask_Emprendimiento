76e0f6ed680cedf2512367452f6d3b64e8264d31
JACC-2006 <adriandejueggos@gmail.com>
2024-11-05 13:44:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, flash, session
+from flask import Flask, render_template, Response, url_for, request, redirect, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 # from flask_sqlalchemy import SQLAlchemy
